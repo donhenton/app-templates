e241de53b80b6f7a2350a8d3b29f66e61b4a06a1
d <dhenton@localhost.com>
2015-03-16 12:45:51 -0500

    template service working

---
+++ jersey-template/src/main/java/com/dhenton9000/jersey/template/config/JerseyConfig.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dhenton9000.jersey.template.config;
+
+import com.dhenton9000.jersey.template.service.DemoService;
+import com.dhenton9000.jersey.template.util.CORSResponseFilter;
+import com.dhenton9000.jersey.template.util.LoggingResponseFilter;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
+
+/**
+ *
+ * @author dhenton
+ */
+public class JerseyConfig extends ResourceConfig {
+
+    /**
+	* Register JAX-RS application components.
+	*/	
+	public JerseyConfig(){
+		register(RequestContextFilter.class);
+		register(DemoService.class);
+		register(JacksonFeature.class);		
+		register(LoggingResponseFilter.class);
+		register(CORSResponseFilter.class);
+	}
+ 
+    
+}
